696e67f4c24cba4bcdd4589880d8e48eece665bb
Jessy Bandya <jessy.bandya5@gmail.com>
2025-07-15 15:52:50 +0300

    Learnt how Alert works on React Native Expo

--- App.js
+++ App.js
@@ -1,13 +1,37 @@
 /*import { StatusBar } from "expo-status-bar";*/
-import { View, StatusBar, Modal, Text, ActivityIndicator} from "react-native";
+import { View, Button, Alert} from "react-native";
 
 export default function App() {
    return (
-    <View style={{ flex: 1, backgroundColor: "white", padding: 60}}>
-    <ActivityIndicator />
-    <ActivityIndicator size= "large" />
-    <ActivityIndicator size= "large" color= "midnightblue"/>
-    <ActivityIndicator size= "large" color= "midnightblue" animating={false}/>
+     <View style={{ flex: 1, backgroundcolor: "plum", padding: 60}}>
+     <Button
+       title= "Alert" 
+       onPress={() => Alert.alert ("Invalid data!")}/>
+     <Button 
+       title= "Alert 2" 
+       onPress={() => Alert.alert ("Invalid data!", "DOB incorrect")}/>
+     <Button 
+       title= "Alert 3" 
+       onPress={() => 
+        Alert.alert ("Invalid data!", "DOB incorect", [
+      {
+        text: "Cancel",
+        onPress: () => console.log ("Cancel pressed"),
+
+     },
+     {
+       text: "Ok",
+        onPress: () => console.log ("Ok pressed"),
+
+     },
+    
+
+     ])
+    }
+    />
+
+     
+
        </View>
   );
 }
