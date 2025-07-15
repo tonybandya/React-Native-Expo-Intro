eb0dec07aff0ff65774af7c4bd444f91c30c0c25
Jessy Bandya <jessy.bandya5@gmail.com>
2025-07-15 12:57:30 +0300

    Learnt how to operate StatusBar on React Native Expo

--- App.js
+++ App.js
@@ -1,25 +1,11 @@
 /*import { StatusBar } from "expo-status-bar";*/
-import { View, Button, Modal, Text } from "react-native";
-import {useState } from "react";
-
+import { View, StatusBar, Modal, Text } from "react-native";
 
 export default function App() {
-  const [isModalVisible, setisModalVisible] = useState(false);
-
-  return (
-    <View style={{ flex: 1, backgroundcolor: "plum", padding: 60}}>
-     <Button 
-       title="click" 
-       onPress={() => setisModalVisible(true)} 
-       color ="midnightblue"
-       /> 
-       <Modal visible={isModalVisible} onRequestClose={() => setisModalVisible(false)} animationType="slide">
-       <View style={{ flex: 1, backgroundcolor: "plum", padding: 60}}>
-          <Text>Modal conent</Text>
-          <Button title="close" color="midnightblue" onPress={() => setisModalVisible(false)} />
-       </View>
-       </Modal>
-    
+   return (
+    <View style={{ flex: 1, backgroundColor: "white", padding: 60}}>
+    <StatusBar backgroundColor= "lightgreen" /*This applies only on Android*/
+     barStyle= "dark-content"/>
     
        </View>
   );
